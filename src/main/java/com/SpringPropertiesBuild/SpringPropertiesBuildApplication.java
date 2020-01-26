bf5dcdc6abfcecff06b10dd69b43f737598b3a2c
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-26 13:27:30 -0500

    initial project added

---
+++ src/main/java/com/SpringPropertiesBuild/SpringPropertiesBuildApplication.java
@@ -0,0 +1,13 @@
+package com.SpringPropertiesBuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertiesBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPropertiesBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringPropertiesBuild/SpringPropertiesBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringPropertiesBuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropertiesBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
