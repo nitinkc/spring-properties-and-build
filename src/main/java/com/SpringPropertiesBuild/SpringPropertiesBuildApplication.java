789797d70897c7e33c462b95d18d3de97244b1a7
Nitin Chaurasia <nitin.chaurasia@davita.com>
2021-04-10 22:39:33 -0500

    app properties to app yml converted

--- src/main/java/com/SpringPropertiesBuild/SpringPropertiesBuildApplication.java
+++ src/main/java/com/SpringPropertiesBuild/SpringPropertiesBuildApplication.java
@@ -1,13 +1,18 @@
 package com.SpringPropertiesBuild;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringPropertiesBuildApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPropertiesBuildApplication.class, args);
+
+		log.info("Application loaded and about to be ended ..........................................");
+		System.exit(0);
 	}
 
 }
